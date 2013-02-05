1af3702a0a3b9e60e92f13b9f466d34e50830a65
Alex Wiltschko <alex.bw@gmail.com>
2013-02-05 18:48:59 -0500

    Gotta start somewhere

---
+++ reduction_test.py
@@ -0,0 +1,15 @@
+import numpy as numpy
+from pycuda import driver, compiler, gpuarray, tools
+
+import pycuda.autoinit
+
+from pycuda.compiler import SourceModule
+mod = SourceModule("""
+    #include <stdio.h>
+    __global__ void say_hi()
+    {
+      printf("I am %d.%d\\n", threadIdx.x, threadIdx.y);
+    }
+    """)
+func = mod.get_function("say_hi")
+func(block=(4,4,1))
