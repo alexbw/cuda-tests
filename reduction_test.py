1fc7df387b3190b839435029dcc05fa54be21853
Matthew Johnson <mattjj@csail.mit.edu>
2013-02-06 19:35:26 -0500

    Added comments and ability to switch L1/shared cache levels

--- reduction_test.py
+++ reduction_test.py
@@ -5,14 +5,21 @@ import time
 from pycuda import driver, compiler, gpuarray, tools
 from pycuda.compiler import SourceModule
 import pycuda.autoinit
+from pycuda.driver import func_cache
+
+preferL1 = True
+if preferL1:
+    pycuda.autoinit.context.set_cache_config(func_cache.PREFER_L1)
+else:
+    pycuda.autoinit.context.set_cache_config(func_cache.PREFER_SHARED)
 
 
-
-
+# Go ahead and grab the kernel code
 with open("test_kernel.cu") as kernel_file:
     kernel_code_template = kernel_file.read()
 
-kernel_code = kernel_code_template.format(message="Awesome!")
+# In this kernel, currently no formatting
+kernel_code = kernel_code_template.format()
 
 # compile the kernel code 
 mod = compiler.SourceModule(kernel_code)
@@ -20,8 +27,6 @@ mod = compiler.SourceModule(kernel_code)
 # get the kernel function from the compiled module
 matrixmul = mod.get_function("MatrixMulKernel")
 
-
-# call the kernel on the card
 
 gpu_speeds = []
 cpu_speeds = []
@@ -37,18 +42,20 @@ for MATRIX_SIZE in [4,8,16]:
 
 
         start = time.time()
+        # Call the kernel on the card
         matrixmul(
             a_gpu,
             c_gpu,
             grid = (GRID_SIZE, GRID_SIZE),
             block = (MATRIX_SIZE, MATRIX_SIZE, 1),
             )
+
+        # Pull the data down from the card (that's part of the benchmark)
         c_gpu.get()
         end = time.time()
-
         print "Execution time {msize}/{gsize}: {t}".format(msize=MATRIX_SIZE, gsize=GRID_SIZE, t=end-start)
 
-
+        
         accumulator = np.zeros((GRID_SIZE, GRID_SIZE))
         cpu_time = time.time()
         for i in range(GRID_SIZE):
@@ -59,9 +66,3 @@ for MATRIX_SIZE in [4,8,16]:
                 cend = cbegin+MATRIX_SIZE
                 accumulator[i,j] = np.sum(a_cpu[rbegin:rend,cbegin:cend])
         print time.time() - cpu_time
-
-
-
-# print "-" * 80
-# print a_gpu.get()
-# 
