2905914727c8853a41382b1c4a3b3db389fd40bb
Alex Wiltschko <alex.bw@gmail.com>
2013-02-13 16:04:25 -0500

    Rasterizing a mouse! And it's pretty fast

--- raster_test.cu
+++ raster_test.cu
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include <algorithm>
 #include <Eigen/Dense>
-
+#include <math.h>
 using namespace Eigen;
 using namespace std;
 
- 
+#define NITER 10000
 #define NVERTS 484
-#define NUMTRIS 940
+#define NFACES 940
 // ==============================
 // Helpers!
 // ==============================
@@ -98,8 +98,46 @@ __device__ inline float getZAtBarycentricCoordinate(Vector3f barycentricCoord, V
 	return barycentricCoord(0)*a(2) + barycentricCoord(1)*b(2) + barycentricCoord(2)*c(2);
 }}
 
+__device__ inline Matrix3f rotateMatrix3D(float rotx, float roty, float rotz) {{
 
-__device__ void rotate(Matrix4f &transform, float rotx, float roty, float rotz) {{
+	rotx = deg2rad(rotx);
+    roty = deg2rad(roty);
+    rotz = deg2rad(rotz);
+
+    float cx = cos(rotx);
+    float sx = sin(rotx);
+    float cy = cos(roty);
+    float sy = sin(roty);
+    float cz = cos(rotz);
+    float sz = sin(rotz);
+
+    Matrix3f Rx = Matrix3f::Identity(); 
+    Matrix3f Ry = Matrix3f::Identity(); 
+    Matrix3f Rz = Matrix3f::Identity();
+    Rx = Rx*cx;
+    Ry = Ry*cy;
+    Rz = Rz*cz;
+
+    Rx(0,0) += 1.0 - cx;
+    Ry(1,1) += 1.0 - cy;
+    Rz(2,2) += 1.0 - cz;
+
+    Rx(1,2) += -sx;
+    Rx(2,1) += sx;
+
+    Ry(0,2) += sy;
+    Ry(2,0) += -sy;
+
+    Rz(0,1) += -sz;
+    Rz(1,0) += sz;
+
+    Matrix3f t;
+    t = Rx*Ry*Rz;
+    return t;
+}}
+
+__device__ inline Matrix4f rotateMatrix(float rotx, float roty, float rotz) {{
+
 	rotx = deg2rad(rotx);
     roty = deg2rad(roty);
     rotz = deg2rad(rotz);
@@ -131,22 +169,45 @@ __device__ void rotate(Matrix4f &transform, float rotx, float roty, float rotz) 
     Rz(0,1) += -sz;
     Rz(1,0) += sz;
 
-    transform = transform*Rx*Ry*Rz;
+    Matrix4f t;
+    t = Rx*Ry*Rz;
+    return t;
 }}
 
-__device__ void translate(Matrix4f &transform, float transx, float transy, float transz)
+__device__ inline void translate(Matrix4f &transform, float transx, float transy, float transz)
 {{
 	transform(0,3) += transx;
-    transform(1,3) += transy;
-    transform(2,3) += transz;
+	transform(1,3) += transy;
+	transform(2,3) += transz;
+}}
+
+__device__ inline Matrix4f translateMatrix(float transx, float transy, float transz)
+{{
+	Matrix4f t = Matrix4f::Identity();
+	t(0,3) = transx;
+    t(1,3) = transy;
+    t(2,3) = transz;
+    return t;
 
 }}
 
-__device__ void scale(Matrix4f &transform, float scalex, float scaley, float scalez)
+__device__ inline Matrix3f scaleMatrix3D(float scalex, float scaley, float scalez)
 {{
-	transform(0,0) *= scalex;
-	transform(1,1) *= scaley;
-	transform(2,2) *= scalez;
+	Matrix3f t = Matrix3f::Identity();
+	t(0,0) = scalex;
+	t(1,1) = scaley;
+	t(2,2) = scalez;
+	return t;
+}}
+
+
+__device__ inline Matrix4f scaleMatrix(float scalex, float scaley, float scalez)
+{{
+	Matrix4f t = Matrix4f::Identity();
+	t(0,0) = scalex;
+	t(1,1) = scaley;
+	t(2,2) = scalez;
+	return t;
 }}
 
 __device__ Matrix4f calcLocalRotation(float rotx, float roty, float rotz,
@@ -207,44 +268,68 @@ __global__ void RasterKernel(GLVertex *vertices,
 							 float resolutionX,
 							 float resolutionY )
 {{
-
-	Vector3f v0(0.0, 0.0, 0.0);
-	Vector3f v1(0.0, 1.0, 0.0);
-	Vector3f v2(1.0, 0.0, 0.0);
-	Matrix4f transform = Matrix4f::Identity();
-	rotate(transform, -90.0, 0.0, 0.0);
-
-	// Matrix4f transform = calcLocalRotation(-90, 0., 0., 0., 0., 0.);
-
-	for (int i=0; i < NVERTS; ++i) {{
-		Vector4f v(vertices[i].x, vertices[i].y, vertices[i].z, 1.0);
-		Vector4f vout = transform*v;
-		vertices[i].x = vout[0];
-		vertices[i].y = vout[1];
-		vertices[i].z = vout[2];
-		// printf("Before: %f. After: %f\n", v[1], vout[1]);
-	}}
-
-	Vector3f a(10., 10., 0.2);
-	Vector3f b(20., 10., 0.);
-	Vector3f c(10., 20., 1.);
-	Vector3f pt(0.5, 0.5, 0.);
-	Vector3f ll = getLowerLeftOfTriangle(a,b,c);
-	Vector3f ur = getUpperRightOfTriangle(a,b,c);
+    
+    Matrix4f mat1 = Matrix4f::Identity();
+    mat1 << 0.8413,  0.0004,  0.471 ,  0.757 ,  0.85  ,  0.4208,  0.72  ,
+        0.0429,  0.1005,  0.8779,  0.9141,  0.3959,  0.7905,  0.7925,
+        0.8939,  0.6818;
+    Matrix4f mat2 = Matrix4f::Identity();
+    mat2 << 0.8413,  0.0004,  0.471 ,  0.757 ,  0.85  ,  0.4208,  0.72  ,
+        0.0429,  0.1005,  0.8779,  0.9141,  0.3959,  0.7905,  0.7925,
+        0.8939,  0.6818;
+    Matrix4f mat3 = Matrix4f::Identity();
+    mat3 << 0.8413,  0.0004,  0.471 ,  0.757 ,  0.85  ,  0.4208,  0.72  ,
+        0.0429,  0.1005,  0.8779,  0.9141,  0.3959,  0.7905,  0.7925,
+        0.8939,  0.6818;
+    #pragma unroll
+    for (int i=0; i<NITER;++i) {{
+        mat1 = mat2.inverse();
+        mat3 = mat1*mat2;
+    }}
 
 
-	for (int i=ll(1); i < ur(1); ++i) {{
-		for (int j=ll(0); j < ur(0); ++j) {{
-			Vector3f pt(i,j,0);
-			Vector3f baryCoord = calcBarycentricCoordinate(pt,a,b,c);
-			bool inTriangle = isBarycentricCoordinateInBounds(baryCoord);
-			if (inTriangle) {{
-				float interpZ = getZAtBarycentricCoordinate(baryCoord,a,b,c);
-				int idx = i*resolutionX + j;
-				depthBuffer[idx] = interpZ;
+	Matrix3f transform = scaleMatrix3D(30.0, 30.0, mat2(0,0))*rotateMatrix3D(-90,0,0);
+	Vector3f transvec(40., 40., 20.);
+
+	// For each triangle, rasterize the crap out of it
+	// (for now, don't care about overlaps)
+	for (int iface=0; iface < NFACES; ++iface) 
+	{{
+		unsigned short i0 = triangles[iface].v0;
+		unsigned short i1 = triangles[iface].v1;
+		unsigned short i2 = triangles[iface].v2;
+
+		Vector3f a(vertices[i0].x, vertices[i0].y, vertices[i0].z);
+		Vector3f b(vertices[i1].x, vertices[i1].y, vertices[i1].z);;
+		Vector3f c(vertices[i2].x, vertices[i2].y, vertices[i2].z);;
+		
+		a = transform*a;
+		a = a+transvec;
+        b = transform*b;
+		b = b+transvec;
+        c = transform*c;
+        c = c+transvec;
+
+
+		Vector3f ll = getLowerLeftOfTriangle(a,b,c);
+		Vector3f ur = getUpperRightOfTriangle(a,b,c);
+
+		for (int i=ll(1); i < ur(1); ++i) {{
+			for (int j=ll(0); j < ur(0); ++j) {{
+				Vector3f pt(j,i,0);
+				Vector3f baryCoord = calcBarycentricCoordinate(pt,a,b,c);
+				bool inTriangle = isBarycentricCoordinateInBounds(baryCoord);
+				if (inTriangle) {{
+					float interpZ = getZAtBarycentricCoordinate(baryCoord,a,b,c);
+					long int idx = i*resolutionX + j;
+					depthBuffer[idx] = interpZ;
+				}}
+				__threadfence();
 			}}
 		}}
+
 	}}
+
 
 
 }}
--- raster_test.py
+++ raster_test.py
@@ -5,6 +5,7 @@ from pycuda.compiler import SourceModule
 import pycuda.autoinit
 from pycuda.driver import func_cache
 from MouseData import MouseData
+from matplotlib.pyplot import *
 
 # Read about this on the "Internets", haven't "tried it" yet.
 import cProfile
@@ -20,7 +21,7 @@ def profileit(func):
 # Cache rules everything around me
 preferL1 = True
 if preferL1:
-    pycuda.autoinit.context.set_cache_config(func_cache.PREFER_L1)
+    pycuda.autoinit.context.set_cache_config(func_cache.PREFER_NONE)
 else:
     pycuda.autoinit.context.set_cache_config(func_cache.PREFER_SHARED)
 
@@ -34,7 +35,8 @@ kernel_code = kernel_code_template.format()
 # compile the kernel code 
 mod = compiler.SourceModule(kernel_code, options=\
                         ['-I/home/dattalab/Code/cuda-tests/include', \
-                        '--compiler-options', '-w' \
+                        '--compiler-options', '-w',
+                        '--optimize', '3', \
                         ], no_extern_c=True)
 
 raster = mod.get_function("RasterKernel")
@@ -43,17 +45,32 @@ raster = mod.get_function("RasterKernel")
 m = MouseData(scenefile="mouse_mesh_low_poly3.npz")
 
 # Put the vertex data on the GPU
-resolutionX = np.float32(30.0)
-resolutionY = np.float32(30.0)
+resolutionX = np.float32(80.0)
+resolutionY = np.float32(80.0)
 depthBuffer_cpu = np.zeros((resolutionX, resolutionY), dtype='float32')
 depthBuffer_gpu = gpuarray.to_gpu(depthBuffer_cpu)
 vert_gpu = gpuarray.to_gpu(m.vertices[:,:3].astype('float32'))
 vert_idx_gpu = gpuarray.to_gpu(m.vertex_idx.astype('uint16'))
-
-raster(vert_gpu, vert_idx_gpu, 
-        depthBuffer_gpu,
-        resolutionX, resolutionY,
-        grid=(1,1,1),
-        block=(1,1,1)
-    )
 driver.Context.synchronize()
+
+for num_blocks in [10]:
+    for num_threads in [128]:        
+        for num_repeats in range(1,5):
+            num_mice = num_blocks*num_threads*num_repeats
+            raster_start = time.time()    
+            for i in range(num_repeats):
+                raster(vert_gpu, vert_idx_gpu, 
+                    depthBuffer_gpu,
+                    resolutionX, resolutionY,
+                    grid=(num_blocks,1,1),
+                    block=(num_threads,1,1)
+                )
+            driver.Context.synchronize()
+            raster_time = time.time() - raster_start
+            print "Rasterized {0} mice/sec [{1}]".format(num_mice/raster_time, num_blocks)
+
+rot_verts = vert_gpu.get()
+depthBuffer = depthBuffer_gpu.get()
+# clf()
+# imshow(depthBuffer)
+# colorbar()
