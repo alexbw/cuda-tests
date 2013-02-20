7405fad633d745d1c42e8a036776ba0513a09e17
Alex Wiltschko <alex.bw@gmail.com>
2013-02-20 03:09:16 -0500

    Typo. Should probably work in dev-rapid branch again...

--- multigpu_raster_test.py
+++ multigpu_raster_test.py
@@ -199,7 +199,7 @@ numMice = min([numMiceFK, numMiceRS, numMiceSK, numMiceLK])
 start = time.time()
 for i,ctx in enumerate(contexts):
     ctx.push()
-    jointRotations_gpu.set(jointRotations_cpu)
+    jointRotations_gpu[i].set(jointRotations_cpu)
 
     #fk
     fk[i](baseJointRotations_gpu[i],
