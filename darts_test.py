f18eef49e1a42066425eed653cada21df90b612c
Alex Wiltschko <alex.bw@gmail.com>
2013-02-25 10:13:36 -0500

    Updated the darts_test.py to the new MousePoser

--- darts_test.py
+++ darts_test.py
@@ -4,22 +4,34 @@ from MousePoser import MousePoser
 import numpy as np
 from pylab import imshow
 
+from MouseData import MouseData
+m = MouseData(scenefile="mouse_mesh_low_poly3.npz")
+mp = MousePoser(mouseModel=m, maxNumBlocks=30)
+numPasses = 2
+ja = np.tile(mp.jointRotations_cpu, (numPasses,1))
+ja[:,0] += np.random.normal(size=(ja.shape[0],), scale=10)
+ja[:,2] += np.random.normal(size=(ja.shape[0],), scale=10)
+scales = np.ones((mp.numMicePerPass*numPasses,3), dtype='float32')
+offsets = np.zeros_like(scales)
+rotations = np.zeros_like(scales)
+
+
 mm = pymouse.Mousemodel("/home/dattalab/hsmm-particlefilters/Test Data", 
-						n=1000,
-						image_size=(64,64))
+                        n=1000,
+                        image_size=(64,64))
 mm.load_data()
 mm.clean_data(normalize_images=False)
 
 
-m = MouseData(scenefile="mouse_mesh_low_poly3.npz")
-mp = MousePoser(mouseModel=m, maxNumBlocks=30)
-ja = np.tile(mp.jointRotations_cpu, (10,1))
-ja[:,0] += np.random.normal(size=(ja.shape[0],), scale=30)
-ja[:,2] += np.random.normal(size=(ja.shape[0],), scale=30)
-p = mp.get_posed_mice(ja)
-
 img = mm.images[35].T[::-1,:].astype('float32')
-l = mp.get_likelihoods(ja, img)
+
+l,p = mp.get_likelihoods(joint_angles=ja, \
+                        scales=scales, \
+                        offsets=offsets, \
+                        rotations=rotations, \
+                        real_mouse_image=img, \
+                        save_poses=True)
+
 
 idx = np.argsort(l)
 best_imgs = np.hstack(p[idx[:5]])
